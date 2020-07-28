faa8801e6290cc553ba620beaddce19b3c7aa91d
Analo-pez <ana.ts.lopez@gmail.com>
2020-07-28 10:39:13 +0200

    add img

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -5,7 +5,7 @@ const PokeList = props => {
     const items = props.pokemons.map((pokemon, index) => {
         return (
             <li key={index} className="App-card">
-                <Pokemon name={pokemon.name} types={pokemon.types} evolution={pokemon.evolution} />
+                <Pokemon name={pokemon.url} name={pokemon.name} types={pokemon.types} evolution={pokemon.evolution} />
             </li>);
     });
 
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -2,13 +2,12 @@ import React from 'react';
 
 const Pokemon = props => {
     return <article className="App-container">
+        <img src={props.url} className="App-img" alt="imagen pokemon" />
         <h1>{props.name}</h1>
         <p>{props.types}</p>
         <span>{props.evolution}</span>
-
-
-
     </article>;
 };
+
 
 export default Pokemon;
